db71cbcc7b946c959b157b0c77068a334204754c
rabiademirbag <rddemirbagg@gmail.com>
2025-05-29 14:59:29 +0300

    Proje dosyası ekle.

---
+++ FirstGenericListExample/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> Guests = new List<string>() { "Bülent Ersoy","Ajda Pekkan","Ebru Gündeş","Hadise","Hande Yener","Tarkan","Funda Arar","Demet Akalın"};
+
+        Console.WriteLine("**Davetliler**");
+        //index numaralarına ihtiyaç duyduğum için for kullandım
+        for(int i=0; i<Guests.Count; i++)
+        {
+            Console.WriteLine(+(i+1) +" - " + Guests[i]);
+        }
+    }
+}
